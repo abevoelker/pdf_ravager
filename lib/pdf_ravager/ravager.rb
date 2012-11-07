ba2b0b072629d61f698c0174c2e2dbf89e4a2b33
Abe Voelker <abe@abevoelker.com>
2012-11-06 22:12:49 -0600

    Fix for certain XFA structures not being populated

--- lib/pdf_ravager/ravager.rb
+++ lib/pdf_ravager/ravager.rb
@@ -43,14 +43,31 @@ module PDFRavager
       # Note: the double-load is to work around a Nokogiri bug I found:
       # https://github.com/sparklemotion/nokogiri/issues/781
       doc = Nokogiri::XML(Nokogiri::XML::Document.wrap(@xfa.getDomDocument).to_xml)
+      node_type = type == :checkbox ? 'integer' : 'text'
       doc.xpath("//*[local-name()='field'][@name='#{name}']").each do |node|
-        # Create an XML node in the XDP like this: "<value><text>#{value}</text></value>"
-        Nokogiri::XML::Builder.with(node) do |xml|
-          xml.value_ {
-            xml.text_ {
-              xml.text value
+        value_node = node.at_xpath("*[local-name()='value']")
+        if value_node
+          text_node = value_node.at_xpath("*[local-name()='#{node_type}']")
+          if text_node
+            # Complete node structure already exists - just set the value
+            text_node.content = value
+          else
+            # <value> node exists, but without child <text> node
+            Nokogiri::XML::Builder.with(value_node) do |xml|
+              xml.text_ {
+                xml.send("#{node_type}_", value)
+              }
+            end
+          end
+        else
+          # No <value> node exists - create whole structure
+          Nokogiri::XML::Builder.with(node) do |xml|
+            xml.value_ {
+              xml.send("#{node_type}_") {
+                xml.text value
+              }
             }
-          }
+          end
         end
       end
       @xfa.setDomDocument(doc.to_java)
@@ -81,11 +98,24 @@ module PDFRavager
     def set_rich_text_field(name, value)
       doc = Nokogiri::XML(Nokogiri::XML::Document.wrap(@xfa.getDomDocument).to_xml)
       doc.xpath("//*[local-name()='field'][@name='#{name}']").each do |node|
-        Nokogiri::XML::Builder.with(node) do |xml|
-          xml.value_ do
+        value_node = node.at_xpath("*[local-name()='value']")
+        if value_node
+          # <value> node exists - just create <exData>
+          Nokogiri::XML::Builder.with(value_node) do |xml|
             xml.exData('contentType' => 'text/html') do
               xml.body_('xmlns' => "http://www.w3.org/1999/xhtml", 'xmlns:xfa' => "http://www.xfa.org/schema/xfa-data/1.0/") do
                 xml << value # Note: this value is not sanitized/escaped!
+              end
+            end
+          end
+        else
+          # No <value> node exists - create whole structure
+          Nokogiri::XML::Builder.with(node) do |xml|
+            xml.value_ do
+              xml.exData('contentType' => 'text/html') do
+                xml.body_('xmlns' => "http://www.w3.org/1999/xhtml", 'xmlns:xfa' => "http://www.xfa.org/schema/xfa-data/1.0/") do
+                  xml << value # Note: this value is not sanitized/escaped!
+                end
               end
             end
           end
