df01d7b5188c3dabe3bdc81634145e41b5d0cf49
Abe Voelker <Abe.Voelker@wicourts.gov>
2014-03-21 15:56:52 -0500

    Update integration specs to RSpec 2 standards

--- spec/integration/multiline_text_field/spec.rb
+++ spec/integration/multiline_text_field/spec.rb
@@ -1,20 +1,20 @@
 require File.dirname(__FILE__) + '/../integration_helper'
 
 describe 'a PDF with a multiline text field' do
-  describe 'filled with FOO' do
+  context 'filled with FOO' do
     before(:each) do
       p = PDFRavager::Template.new do |t|
         t.text 'multilinetext', 'FOO' * 10000
       end
       pdf_file = File.join(File.dirname(__FILE__), "pdf")
       @pdf_file = mktemp('.pdf')
-      @pdf = p.ravage pdf_file, :out_file => @pdf_file
+      p.ravage pdf_file, :out_file => @pdf_file
     end
 
-    it 'should match expected.png when rendered' do
+    it 'matches expected.png when rendered' do
       png = pdf_to_png(@pdf_file)
       pix_diff, pct_diff = png_diff(png, File.join(File.dirname(__FILE__), "expected.png"))
-      pix_diff.should == 0
+      expect(pix_diff).to eq(0)
     end
   end
 end
--- spec/integration/text_field/spec.rb
+++ spec/integration/text_field/spec.rb
@@ -1,20 +1,20 @@
-require 'integration/integration_helper'
+require File.dirname(__FILE__) + '/../integration_helper'
 
 describe 'a PDF with a text field' do
-  describe 'filled with foo' do
+  context 'filled with foo' do
     before(:each) do
       p = PDFRavager::Template.new do |t|
         t.text 'text_field', 'foo'
       end
       pdf_file = File.join(File.dirname(__FILE__), "pdf")
       @pdf_file = mktemp('.pdf')
-      @pdf = p.ravage pdf_file, :out_file => @pdf_file
+      p.ravage pdf_file, :out_file => @pdf_file
     end
 
-    it 'should match expected.png when rendered' do
+    it 'matches expected.png when rendered' do
       png = pdf_to_png(@pdf_file)
       pix_diff, pct_diff = png_diff(png, File.join(File.dirname(__FILE__), "expected.png"))
-      pix_diff.should be 0
+      expect(pix_diff).to eq(0)
     end
   end
 end
