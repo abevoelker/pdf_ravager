02a23857f65b8b2a6077e54e5316c3b0335bf9f7
Abe Voelker <Abe.Voelker@wicourts.gov>
2014-04-02 09:09:46 -0500

    Fix for rich text not setting value properly

--- lib/pdf_ravager/field_types/xfa.rb
+++ lib/pdf_ravager/field_types/xfa.rb
@@ -16,48 +16,27 @@ module PDFRavager
         @value.to_s
       end
 
-      def set_xfa_value(xfa)
-        # the double-load is to work around a Nokogiri bug I found:
-        # https://github.com/sparklemotion/nokogiri/issues/781
-        doc = Nokogiri::XML(Nokogiri::XML::Document.wrap(xfa.getDomDocument).to_xml)
-        # first, assume the user-provided field name is an xpath and use it directly:
-        strict_match =
-          begin
-            doc.xpath(xfa_name)
-          rescue Nokogiri::XML::XPath::SyntaxError
-            []
-          end
-        # otherwise, we'll loosely match the field name anywhere in the document:
-        loose_match = doc.xpath("//*[local-name()='field'][@name='#{xfa_name}']")
-        matched_nodes = strict_match.any? ? strict_match : loose_match
-        matched_nodes.each do |node|
-          value_node = node.at_xpath("*[local-name()='value']")
-          if value_node
-            text_node = value_node.at_xpath("*[local-name()='#{xfa_node_type}']")
-            if text_node
-              # Complete node structure already exists - just set the value
-              text_node.content = xfa_value
-            else
-              # <value> node exists, but without child <text> node
-              Nokogiri::XML::Builder.with(value_node) do |xml|
-                xml.text_ {
-                  xml.send("#{xfa_node_type}_", xfa_value)
-                }
-              end
-            end
+      def set_xfa_value(node)
+        value_node = node.at_xpath("*[local-name()='value']")
+        if value_node
+          child_node = value_node.at_xpath("*[local-name()='#{xfa_node_type}']")
+          if child_node
+            # Complete node structure already exists - just set the value
+            child_node.content = xfa_value
           else
-            # No <value> node exists - create whole structure
-            Nokogiri::XML::Builder.with(node) do |xml|
-              xml.value_ {
-                xml.send("#{xfa_node_type}_") {
-                  xml.text xfa_value
-                }
-              }
+            # Must create child <#{xfa_node_type}> node
+            Nokogiri::XML::Builder.with(value_node) do |xml|
+              xml.send("#{xfa_node_type}_", xfa_value)
             end
+          end
+        else
+          # No <value> node exists - create whole structure
+          Nokogiri::XML::Builder.with(node) do |xml|
+            xml.value_ {
+              xml.send("#{xfa_node_type}_", xfa_value)
+            }
           end
         end
-        xfa.setDomDocument(doc.to_java)
-        xfa.setChanged(true)
       end
 
     end
--- lib/pdf_ravager/fields/rich_text.rb
+++ lib/pdf_ravager/fields/rich_text.rb
@@ -15,46 +15,19 @@ module PDFRavager
         self.name == other.name && self.value == other.value
       end
 
-      def set_xfa_value(xfa)
-        # the double-load is to work around a Nokogiri bug I found:
-        # https://github.com/sparklemotion/nokogiri/issues/781
-        doc = Nokogiri::XML(Nokogiri::XML::Document.wrap(xfa.getDomDocument).to_xml)
-        # first, assume the user-provided field name is an xpath and use it directly:
-        strict_match =
-          begin
-            doc.xpath(xfa_name)
-          rescue Nokogiri::XML::XPath::SyntaxError
-            []
-          end
-        # otherwise, we'll loosely match the field name anywhere in the document:
-        loose_match = doc.xpath("//*[local-name()='field'][@name='#{xfa_name}']")
-        matched_nodes = strict_match.any? ? strict_match : loose_match
-        matched_nodes.each do |node|
-          value_node = node.at_xpath("*[local-name()='value']")
-          if value_node
-            # <value> node exists - just create <exData>
-            Nokogiri::XML::Builder.with(value_node) do |xml|
-              xml.exData('contentType' => 'text/html') do
-                xml.body_('xmlns' => "http://www.w3.org/1999/xhtml", 'xmlns:xfa' => "http://www.xfa.org/schema/xfa-data/1.0/") do
-                  xml << xfa_value # Note: this value is not sanitized/escaped!
-                end
-              end
-            end
-          else
-            # No <value> node exists - create whole structure
-            Nokogiri::XML::Builder.with(node) do |xml|
-              xml.value_ do
-                xml.exData('contentType' => 'text/html') do
-                  xml.body_('xmlns' => "http://www.w3.org/1999/xhtml", 'xmlns:xfa' => "http://www.xfa.org/schema/xfa-data/1.0/") do
-                    xml << xfa_value # Note: this value is not sanitized/escaped!
-                  end
-                end
-              end
-            end
-          end
+      def set_xfa_value(node)
+        value_node = node.at_xpath("*[local-name()='value']")
+        value_node && value_node.remove # we will replace the whole <value> node
+        Nokogiri::XML::Builder.with(node) do |xml|
+          xml.value_ {
+            xml.exData('contentType' => 'text/html') {
+              xml.body_('xmlns'     => "http://www.w3.org/1999/xhtml",
+                        'xmlns:xfa' => "http://www.xfa.org/schema/xfa-data/1.0/") {
+                xml << xfa_value
+              }
+            }
+          }
         end
-        xfa.setDomDocument(doc.to_java)
-        xfa.setChanged(true)
       end
 
     end
